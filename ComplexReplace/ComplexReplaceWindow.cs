f00e5b09f6046f65cd086a5bb2582fcc7cf98e53
Mikhail Naumov <mikhail_naumov@yahoo.com>
2017-10-23 10:46:58 -0700

    first commit

---
+++ ComplexReplace/ComplexReplaceWindow.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ComplexReplaceWindow.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace ComplexReplace
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("444baeb1-8245-4fe9-9b5d-de66ea6c3053")]
+    public class ComplexReplaceWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComplexReplaceWindow"/> class.
+        /// </summary>
+        public ComplexReplaceWindow() : base(null)
+        {
+            this.Caption = "Complex Replace";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ComplexReplaceWindowControl();
+        }
+    }
+}
---
+++ ComplexReplace/ComplexReplaceWindowControl.xaml.cs
@@ -0,0 +1,99 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ComplexReplaceWindowControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Composition;
+using System.Windows;
+using System.Windows.Controls;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text;
+
+namespace ComplexReplace
+{
+    public partial class ComplexReplaceWindowControl : UserControl
+    {
+        #region Fields
+        private TextDocument _document;
+        #endregion
+
+        #region Properties
+        [Import]
+        public ITextDocumentFactoryService textDocumentFactory { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ComplexReplaceWindowControl" /> class.
+        /// </summary>
+        public ComplexReplaceWindowControl()
+        {
+            InitializeComponent();
+            btnUndo.IsEnabled = false;
+
+            txtFind.Text = Properties.Settings.Default.Find;
+            txtReplace.Text = Properties.Settings.Default.Replace;
+        }
+        #endregion
+
+        #region Methods
+        private void btnReplace_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Find = txtFind.Text;
+            Properties.Settings.Default.Replace = txtReplace.Text;
+            Properties.Settings.Default.Save();
+
+            var doc = (Package.GetGlobalService(typeof (DTE)) as DTE).ActiveDocument;
+            if (doc == null)
+            {
+                MessageBox.Show("No file opened");
+                return;
+            }
+
+            _document = doc.Object() as TextDocument;
+            
+            var findStrCollection = txtFind.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var replaceStrCollection = txtReplace.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            btnUndo.IsEnabled = FindAndReplace(findStrCollection, replaceStrCollection);
+        }
+
+        private void btnUndo_Click(object sender, RoutedEventArgs e)
+        {
+            var doc = (Package.GetGlobalService(typeof (DTE)) as DTE).ActiveDocument;
+            var txtDoc = doc.Object() as TextDocument;
+            if (txtDoc == _document)
+            {
+                var findStrCollection = txtFind.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                var replaceStrCollection = txtReplace.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                btnUndo.IsEnabled = !FindAndReplace(replaceStrCollection, findStrCollection);
+            }
+        }
+
+        private bool FindAndReplace(string[] findStrCollection, string[] replaceStrCollection)
+        {
+            if (findStrCollection.Length == 0)
+            {
+                MessageBox.Show("No search arguments provided");
+                return false;
+            }
+
+            if (findStrCollection.Length != replaceStrCollection.Length)
+            {
+                MessageBox.Show("Number of search and replace arguments mismatch");
+                return false;
+            }
+
+            for (var i = 0; i < findStrCollection.Length; i++)
+            {
+                var editPnt = _document.StartPoint.CreateEditPoint();
+                editPnt.ReplacePattern(_document.EndPoint, findStrCollection[i], replaceStrCollection[i]);
+            }
+
+            return true;
+        }
+        #endregion
+    }
+}
